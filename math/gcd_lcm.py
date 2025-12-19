a4ae8f020358701ab80992c6044fa0cd0b192011
mang330 <mang3330@gmail.com>
2025-12-19 18:13:39 +0900

    Add gcd and lcm utilities

---
+++ math/gcd_lcm.py
@@ -0,0 +1,14 @@
+﻿def gcd(a, b):
+    a, b = abs(a), abs(b)
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+def lcm(a, b):
+    if a == 0 or b == 0:
+        return 0
+    return abs(a // gcd(a, b) * b)
+
+if __name__ == "__main__":
+    print(gcd(24, 18))
+    print(lcm(24, 18))
