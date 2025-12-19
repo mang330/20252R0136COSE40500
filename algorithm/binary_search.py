601367a4efc41f0f6bf9f50360e4e9988a784512
mang330 <mang3330@gmail.com>
2025-12-19 18:13:11 +0900

    Add binary search implementation

---
+++ algorithm/binary_search.py
@@ -0,0 +1,16 @@
+﻿def binary_search(sorted_arr, target):
+    lo, hi = 0, len(sorted_arr) - 1
+    while lo <= hi:
+        mid = (lo + hi) // 2
+        if sorted_arr[mid] == target:
+            return mid
+        if sorted_arr[mid] < target:
+            lo = mid + 1
+        else:
+            hi = mid - 1
+    return -1
+
+if __name__ == "__main__":
+    arr = [1, 3, 4, 7, 9, 12]
+    print(binary_search(arr, 7))
+    print(binary_search(arr, 2))
