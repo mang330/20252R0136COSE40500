6bf4f5e7a5b1c72246c06cf312073febedb55fd9
mang330 <mang3330@gmail.com>
2025-12-19 18:12:47 +0900

    Add bubble sort implementation

---
+++ algorithm/bubble_sort.py
@@ -0,0 +1,15 @@
+﻿def bubble_sort(arr):
+    a = arr[:]  # do not mutate input
+    n = len(a)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n - 1 - i):
+            if a[j] > a[j + 1]:
+                a[j], a[j + 1] = a[j + 1], a[j]
+                swapped = True
+        if not swapped:
+            break
+    return a
+
+if __name__ == "__main__":
+    print(bubble_sort([5, 1, 4, 2, 8]))
