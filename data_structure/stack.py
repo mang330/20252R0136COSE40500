86a18e914a5e1d8a0a0fe601e90e1bf7f46aa389
mang330 <mang3330@gmail.com>
2025-12-19 18:13:16 +0900

    Add stack data structure

---
+++ data_structure/stack.py
@@ -0,0 +1,28 @@
+﻿class Stack:
+    def __init__(self):
+        self._data = []
+
+    def push(self, x):
+        self._data.append(x)
+
+    def pop(self):
+        if not self._data:
+            raise IndexError("pop from empty stack")
+        return self._data.pop()
+
+    def peek(self):
+        if not self._data:
+            raise IndexError("peek from empty stack")
+        return self._data[-1]
+
+    def is_empty(self):
+        return len(self._data) == 0
+
+    def __len__(self):
+        return len(self._data)
+
+if __name__ == "__main__":
+    s = Stack()
+    s.push(10); s.push(20)
+    print(s.pop())
+    print(s.peek())
